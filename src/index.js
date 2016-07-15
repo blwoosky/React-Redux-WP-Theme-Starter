0e62b49f8d2282a86934d9ec83cf1d18dc757d43
BlwooSky <blwoosky>
2016-07-15 08:40:22 +0800

    add index.js file

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { browserHistory } from 'react-router';
+import { Provider } from 'react-redux';
+import { Router, Route, IndexRoute } from 'react-router';
+import configureStore from './store/configureStore';
+import Main from "./components/Main";
+
+const store = configureStore();
+
+import "./style/style.css";
+
+ReactDOM.render(
+    <Provider store={store}>
+        <Router history={browserHistory}>
+            <Route path="/" component={Main}>
+
+            </Route>
+        </Router>
+    </Provider>,
+    document.getElementById('root')
+);
